3bc1551327c1fb71661785ed3276b14b2bb8666b
suziwen <suziwen1@gmail.com>
2025-04-23 07:57:27 +0800

    本地自动启动执行脚本

--- client_server_run.sh
+++ client_server_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 . ~/.nvm/nvm.sh
 nvm use 18.16.0
-cd src
-http-server -p 2022 -c600
+pm2 delete jopp-stores-2022-server
+pm2 start http-server --name "jopp-stores-2022-server" -- -p 2022 -c600 src
+
+#cd src
+#http-server -p 2022 -c600
